8625f388e3af59e66f537dac126d11d70a532354
fatimaknz <ambariknzzz7@gmail.com>
2022-06-26 16:41:23 +0530

    commit

---
+++ ecom/shop/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Product
+from .models import Contact,Order,OrderUpdate
+
+
+
+admin.site.register(Contact)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderUpdate)
